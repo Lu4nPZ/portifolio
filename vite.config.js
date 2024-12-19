88688780a9e037af833ad9dc808371936e283e3f
Lu4nPZ <luannovaes152@gmail.com>
2024-12-18 22:03:21 -0300

    Atualizado os dados do portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Configuração do Vite
 export default defineConfig({
+  base: '/portfolio/', // Substitua pelo nome do seu repositório
   plugins: [react()],
-})
+});
